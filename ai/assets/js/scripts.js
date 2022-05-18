38bb0647f709f214f18869efdd321fdb700c644b
Barbara Esteves <104402057+TBadWolf@users.noreply.github.com>
2022-05-17 21:03:46 -0300

    Update scripts.js

--- ai/assets/js/scripts.js
+++ ai/assets/js/scripts.js
@@ -1,5 +1,6 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber =0;
+alert ("vamos contar um por um?");
 function increment (){
   currentNumber = currentNumber + 1;
   currentNumberWrapper.innerHTML = currentNumber;
